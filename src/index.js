c99b37b9f7a549dcd6dd311dad15788387d150cc
cailyncon <77636418+cailyncon@users.noreply.github.com>
2023-12-15 10:33:17 -0800

    replaced js file

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
-function displayTemperature(response) {
-  let temperatureElement = document.querySelector("#current-temperature");
-  let temperature = Math.round(response.data.temperature.current);
-  let cityElement = document.querySelector("#current-city");
-  cityElement.innerHTML = response.data.city;
-  temperatureElement.innerHTML = temperature;
-}
-
 function search(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-input");
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = searchInputElement.value;
   let city = searchInputElement.value;
-
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-
+  let apiKey = "b84b065do096ab8b5a39fb9t38e99a64";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayTemperature);
 }
 
@@ -51,3 +43,9 @@ let currentDateELement = document.querySelector("#current-date");
 let currentDate = new Date();
 
 currentDateELement.innerHTML = formatDate(currentDate);
+
+function displayTemperature(response) {
+  let temperature = Math.round(response.data.temperature.current);
+  let tempElement = document.querySelector("#current-temp");
+  tempElement.innerHTML = `${temperature}`;
+}
