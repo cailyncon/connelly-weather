a33501dcadf32c8c9fde3c6f3b845572c80f778d
cailyncon <77636418+cailyncon@users.noreply.github.com>
2023-12-27 14:13:21 -0800

    add weather forecast html + js

--- src/index.js
+++ src/index.js
@@ -64,5 +64,34 @@ function displayTemperature(response) {
 }
 
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecastHtml = "";
+
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
+      <div class="forecast">
+        <div>  
+          <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
+        </div>
+
+        <div class="weather-forecast-date">${formatDay(day.time)}</div>
+
+        <div class="weather-forecast-temperatures">
+          <div class="weather-forecast-temperature">
+            ${Math.round(day.temperature.maximum)}º</
+          </div>
+          <div class="weather-forecast-temperature">${Math.round(
+            day.temperature.minimum
+          )}º</div>
+        </div>
+        
+      </div>
+    `;
+    }
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
 }
